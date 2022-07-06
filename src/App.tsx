4de13ec0b45697d497dabd3786dc5870df34a9d1
Krzysztof Stankiewicz <krzystan@wp.pl>
2022-07-06 21:16:53 +0200

    few changes in layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
 import Header from './components/Header/Header';
 import Footer from "./components/Footer/Footer";
-
 import Article from "./components/Article/Article";
 
 function App() {
   return (
       <div className="App">
         <Header/>
-
         <Article/>
         <Footer/>
       </div>
