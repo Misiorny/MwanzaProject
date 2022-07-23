9e8a0612736ee5efa25874b15aeb31b63f81da1a
Krzysztof Stankiewicz <krzystan@wp.pl>
2022-07-23 18:43:31 +0200

    feat:few modifications in CSS and Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from '../../style/styles.module.scss';
 function Header() {
   return (
       <header className={styles.header}>
-        <div className={styles.shadow}></div>
+
         <h1 className={styles.header__title } >The Mwanza Project</h1>
         <p className={styles.header__text}>We can help together!</p>
         <Navbar/>
